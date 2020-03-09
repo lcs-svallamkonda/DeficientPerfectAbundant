942ac0d5fd99284a16489726851413c6f06ccfb4
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2020-03-09 11:29:57 -0400

    Works for all input except for 1

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -5,48 +5,56 @@
 //  Created by Vallamkonda, Sunaina on 2020-03-09.
 //  Copyright © 2020 Vallamkonda, Sunaina. All rights reserved.
 //
-var n = 0
 
 import Foundation
 
 while true {
     
-    print("Type an integer between 1 and 32 500")
+    var n = 0
     
-    guard let input = readLine() else {
+    
+    
+    while true {
         
-        //No input was provided
-        continue
-    }
-    
-    guard let integer = Int(input) else {
+        print("Type an integer between 1 and 32 500")
         
-        //input is not an integer
-        continue
+        guard let input = readLine() else {
+            
+            //No input was provided
+            continue
+        }
+        
+        guard let integer = Int(input) else {
+            
+            //input is not an integer
+            continue
+        }
+        
+        guard integer > 0, integer < 32_501 else {
+            continue
+        }
+        n = integer
+        
+        break
     }
     
-    guard integer > 0, integer < 32_501 else {
-        continue
-    }
-    n = integer
+    var total = 0
     
-    break
+    for number in 0...n - 1 {
+        //find the proper divisors of n
+        if n % number == 0 {
+            total += number
+        }
+        
+    }
+    
+    print(total)
+    
+    if total < n {
+        print("\(n) is a deficient number")
+    } else if total == n {
+        print("\(n) is a perfect number")
+    } else {
+        print("\(n) is a abundant number")
+    }
 }
-
-var total = 0
-
-for number in 1...n - 1 {
-    //find the proper divisors of n
-    if n % number == 0 {
-        total += number
-    }
-    
-}
-
-print(total)
-
-//if total < n {
-//    print("\(n) is a deficient number")
-//} else if total == n {
-//    print("\()")
-//}
