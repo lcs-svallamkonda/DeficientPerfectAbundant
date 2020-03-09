9439e84aee34892dd088b62ed7d6b54f7cd6ecfd
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2020-03-09 11:05:25 -0400

    Initial Commit

---
+++ DeficientPerfectAbundant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeficientPerfectAbundant
+//
+//  Created by Vallamkonda, Sunaina on 2020-03-09.
+//  Copyright © 2020 Vallamkonda, Sunaina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
