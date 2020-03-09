dfb6f4dc77edc1f0cdf60e565fb822df1a876ce9
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2020-03-09 11:13:54 -0400

    finished guard statements for integer given

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -5,8 +5,30 @@
 //  Created by Vallamkonda, Sunaina on 2020-03-09.
 //  Copyright © 2020 Vallamkonda, Sunaina. All rights reserved.
 //
+var n = 0
 
 import Foundation
 
-print("Hello, World!")
-
+while true {
+    
+    print("Type an integer between 1 and 32 500")
+    
+    guard let input = readLine() else {
+        
+        //No input was provided
+        continue
+    }
+    
+    guard let integer = Int(input) else {
+        
+        //input is not an integer
+        continue
+    }
+    
+    guard integer > 0, integer < 32_501 else {
+        continue
+    }
+    n = integer
+    
+    break
+}
